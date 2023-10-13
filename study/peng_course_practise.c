f6bd8497037898cec29793bbbdaf7790fe351b2b
northtruths <zp1716120350@gmail.com>
2023-10-13 20:03:38 +0800

    仓库更新/练习

---
+++ study/peng_course_practise.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{int num1;int num2;
+//printf("请输入两个整数：");
+//scanf("%d %d", &num1, &num2);
+//int sum=num1+num2;
+//printf("%d和%d的和为:%d",num1,num2,sum);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf('\a');
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hehe");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d\n%d", &a, &b);
+//	printf("%d %d",a,b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	float b = 0;
+//	scanf("%d", &a);
+//	printf("%d\n", a);
+//	scanf("%f", &b);
+//	printf("%.2f", b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (i = 5)
+//			printf("%d", i);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	a = 4;
+//	c = ++a;
+//	b = ++c,c++,++a,a++;
+//	printf("%d\n%d\n%d", a, b, c);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	
+//	printf("%.2f\n%d\n%d\n%d\n%d", 7.0/ 2,6,6,6);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d", a);
+//	return 0;
+//}
---
+++ study/school.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	int max(int, int);
+//	printf("请输入一个整数：");
+//	scanf("%d", &a);
+//	printf("请再输入一个整数：");
+//	scanf("%d", &b);
+//	c = max(a, b);
+//	printf("%d和%d中最大的为%d", a, b, c);
+//	return 0;
+//}
+//int max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
